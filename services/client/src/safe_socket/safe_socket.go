a61820b59e4f575ded51fc3e7308b4384bee9d91
Ignacio Carrera <carrera.ignacio05@gmail.com>
2026-08-26 15:43:30 -0300

    Ejercicio 4, implementacion de funciones de comunicacion para el manejo de posibles errores (short read y short write)

--- services/client/src/safe_socket/safe_socket.go
+++ services/client/src/safe_socket/safe_socket.go
@@ -5,18 +5,39 @@ import "io"
 //TODO: Complete with a short-read/short-write tolerant implementation
 
 func SendAll(socket io.Writer, bytes []byte) error {
-	_, err := socket.Write(bytes)
-	if err != nil {
-		return err
+	writtenCount := 0
+
+	for writtenCount < len(bytes) {
+		n, err := socket.Write(bytes[writtenCount:])
+
+		writtenCount += n
+
+		if err != nil {
+			return err
+		}
 	}
 	return nil
 }
 
 func RecvAll(socket io.Reader, size int) ([]byte, error) {
 	buff := make([]byte, size)
-	n, err := socket.Read(buff)
-	if err != nil {
-		return nil, err
+
+	readCount := 0
+	
+	// leo hasta alcanzar los bytes que quiere la funcion
+	for readCount < size {
+
+		n, err := socket.Read(buff[readCount:])
+		
+		readCount += n
+ 
+		if err != nil {
+			if err == io.EOF && readCount == size { // termine de leer
+				return buff, nil
+			}
+			return nil, err
+		}
+	
 	}
-	return buff[:n], nil
+	return buff, nil
 }
--- services/server/src/safe_socket/safe_socket.py
+++ services/server/src/safe_socket/safe_socket.py
@@ -4,8 +4,31 @@ import socket
 
 
 def recv_all(socket: socket.socket, size):
-    return socket.recv(size)
+
+    if size < 0:
+        raise ValueError("size must be equal or greater than cero")
+
+    received = bytearray()
+
+    while len(received) < size:
+
+        pending = size - len(received)
+        to_recv = socket.recv(pending)
+
+        if to_recv == b"":
+            raise ConnectionError("Connection closed by the other side")
+
+        received.extend(to_recv)
+
+    return bytes(received)
 
 
 def send_all(socket: socket.socket, bytes):
-    return socket.send(bytes)
+
+    total = 0
+
+    while total < len(bytes):
+        sent = socket.send(bytes[total:])
+        total += sent
+
+    return total
